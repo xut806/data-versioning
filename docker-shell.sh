76aade498190871babce409d0d48c3f72e0c5ac2
Xu Tang <xutang@g.harvard.edu>
2024-11-03 22:04:23 -0500

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,7 +4,7 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
+export GCS_BUCKET_NAME="cheese-app-data-versioning-215"
 export GCP_PROJECT="ac215-project"
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
